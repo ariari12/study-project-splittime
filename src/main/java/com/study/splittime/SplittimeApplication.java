bcf9a972080c7e888779563920173781086af965
kah <rladkgus4553@gmail.com>
2023-10-07 04:22:38 +0900

    환경설정

---
+++ src/main/java/com/study/splittime/SplittimeApplication.java
@@ -0,0 +1,13 @@
+package com.study.splittime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplittimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplittimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/splittime/SplittimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.splittime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplittimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
