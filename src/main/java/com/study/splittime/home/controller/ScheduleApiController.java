e61aa8797aa0e16d41aa78ec696400531b2543c4
kah <rladkgus4553@gmail.com>
2023-11-02 23:14:08 +0900

    Controller로 변경

--- src/main/java/com/study/splittime/home/controller/ScheduleApiController.java
+++ src/main/java/com/study/splittime/home/controller/ScheduleApiController.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @Slf4j
 @RequestMapping("/home")
 @RequiredArgsConstructor
--- src/main/java/com/study/splittime/todo/controller/ToDoApiController.java
+++ src/main/java/com/study/splittime/todo/controller/ToDoApiController.java
@@ -3,12 +3,12 @@ package com.study.splittime.todo.controller;
 import com.study.splittime.todo.model.ToDoDto;
 import com.study.splittime.todo.service.ToDoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-@RestController
+@Controller
 @RequestMapping("/home")
 @RequiredArgsConstructor
 public class ToDoApiController {
